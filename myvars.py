f65a6ff4b8c005cec28adde4813c97cbb1f1305b
sruli <sruli@saurymper.com>
2020-11-30 15:26:28 +0000

    typo

--- myvars.py
+++ myvars.py
@@ -1,4 +1,4 @@
 # This file is for pushing updates only! copy to _myvars and place real values there!
 DNSBL_ZONE = 'dns.private.bl'
 DNS_HOSTNAME = 'dns.private.bl'
-RNDC_KEY = {'private.bl_rndc-key.' : 'yourkeyhere=='}
+RNDC_KEY = {'dns.private.bl_rndc-key.' : 'yourkeyhere=='}
